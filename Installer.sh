66a3499fff219f0875d5dae036eff222a1944cf6
Gianguido <gianguidorama@gmail.com>
2011-04-30 21:34:23 +0200

    replaced by make

--- Installer.sh
+++ Installer.sh
@@ -1,12 +1,15 @@
-mkdir ~/.MacOSX &>/dev/null
-touch ~/.MacOSX/environment.plist
-echo "Compiling StarLoader"
-make
-cp Loader.dylib ~/.MacOSX
-cd
-cat > ~/.MacOSX/environment.plist << EOF
-{
-	"DYLD_INSERT_LIBRARIES" = "$PWD/.MacOSX/Loader.dylib";
-}
-EOF
-echo "Done. Logout and enjoy!"
+# I'm obsolete, i do nothing ^^
+#
+#
+#mkdir ~/.MacOSX &>/dev/null
+#touch ~/.MacOSX/environment.plist
+#echo "Compiling StarLoader"
+#make
+#cp Loader.dylib ~/.MacOSX
+#cd
+#cat > ~/.MacOSX/environment.plist << EOF
+#{
+#	"DYLD_INSERT_LIBRARIES" = "$PWD/.MacOSX/Loader.dylib";
+#}
+#EOF
+#echo "Done. Logout and enjoy!"
